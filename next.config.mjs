9aa0b4a8284a3d1f441021168156c9769988172f
Nam Nguyen <namnguyen30500@gmail.com>
2024-10-06 21:49:23 +0700

    setup repository

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import AutoImport from "unplugin-auto-import/webpack";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.plugins.push(
+      AutoImport({
+        imports: ["react"],
+        // Tự động import các components của React
+        dirs: ["./src/components/ui"], // Quét thư mục components/ui để import tự động
+        dts: true, // Tạo file khai báo tự động cho TypeScript
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
